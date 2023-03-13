45828ae7bc3168689282beb3bd88d228eacde1e6
Victor <manuelher1619@gmail.com>
2023-03-12 23:35:56 -0600

    Agregue luna en footer

--- js/footer.js
+++ js/footer.js
@@ -17,9 +17,7 @@ function pieDePag() {
     <li class="ms-3 justify-content-center py-1  " ><a class="text-muted" href="https://www.facebook.com/maodantefoto/about"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
       </svg></a></li>
-      <a class="moon">
-      <span class="mb-3 mb-md-0 text-white" class="moon">Created by Digital Moon </span> <img class="luna" scr="/imagenes/LOGO12.png"> 
-      </a>
+     
       <div id="whatsapp">
     <a href="https://api.whatsapp.com/send?phone=agregarnumeroaqui&text=Hola,%20Gracias%20por%20su%20interes%20¿En%20qué%20le%20puedo%20ayudar?" target="_blank">
         <svg width="80" height="80" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
@@ -32,7 +30,14 @@ function pieDePag() {
     </a>
     
   </div>
+  
   </ul>
+  <a class="moon">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
+  <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
+</svg>
+      <span class="mb-3 mb-md-0 text-white" class="moon">Created by Digital Moon </span> <img class="luna" scr="/imagenes/LOGO12.png"> 
+      </a>
 </footer>
 `;
 }
